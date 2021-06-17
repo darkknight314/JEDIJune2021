833f53cfd199c8f5bee692c8832fe4b87a506958
darkknight314 <pratik.kakade7@gmail.com>
2021-06-17 10:17:38 +0530

    Added necessary files

---
+++ CCode/addition.c
@@ -0,0 +1,9 @@
+#inlcude <stdio.h>
+
+int sum(int x, int y) {
+	return x + y
+}
+
+int sum3(int x, int y, int z) {
+	return x + y + z
+}
---
+++ CCode/helloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio>
+
+void helloWorld();
+
+int main() {
+	helloWorld();
+}
+
+void helloWorld() {
+	printf("Hello World!");
+	printf("Awesome World!!!");
+}
+
